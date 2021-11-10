85d380f4622ef69317687d7565f93abd4529d8f5
haitam0777 <haitamdhaibi@gmail.com>
2021-11-10 15:56:23 +0100

    first commit

---
+++ VolumeHand.py
@@ -0,0 +1,69 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import time
+import HandTrackingModule as htm
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+pTime = 0
+cTime = 0
+cap = cv2.VideoCapture(0)
+detector = htm.handDetector(detectionCon=0.8)
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+VolRange = volume.GetVolumeRange()
+
+minVol=VolRange[0]
+maxVol=VolRange[1]
+
+
+
+
+vol = 0
+volBar = 400
+volPer = 0
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img, draw=True )
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+
+        x1,y1 = lmList[4][1],lmList[4][2]
+        x2,y2 = lmList[8][1],lmList[8][2]
+        cx,cy = (x1+x2)//2,(y1+y2)//2
+
+        cv2.circle(img,(x1,y1),15,(255,8,255),cv2.FILLED)
+        cv2.circle(img,(x2,y2),15,(255,8,255),cv2.FILLED)
+        cv2.circle(img,(cx,cy),15,(255,8,255),cv2.FILLED)
+        cv2.line(img,(x1,y1),(x2,y2),(255,8,255),3)
+
+        length = math.hypot(x2-x1,y2-y1)
+
+        vol = np.interp(length,[15,235],[minVol,maxVol])
+        volBar = np.interp(length, [15,235], [400, 150])
+        volPer = np.interp(length, [15,235], [0, 100])
+        print(int(length), vol)
+        print(vol)
+        volume.SetMasterVolumeLevel(vol, None)
+        if length < 30:
+            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+    cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
+    cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
+                1, (255, 0, 0), 3)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                (255, 0, 255), 3)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
